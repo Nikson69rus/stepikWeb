8bc0826d617c5d4adb8a97494008f184b6657f2f
Nikson69 <Nikita@nikson69.ru>
2018-03-28 01:20:57 +0300

    L2.1 и порядок
    L2.1 и порядок

---
+++ L2.2/src/main/java/servlets/UsersServlet.java
@@ -0,0 +1,60 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class UsersServlet extends HttpServlet {
+    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"}) //todo: remove after module 2 home work
+    private final AccountService accountService;
+
+    public UsersServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //get public user profile
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+
+    //sign up
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        accountService.addNewUser(new UserProfile(login, pass, login));
+
+    }
+
+    //change profile
+    public void doPut(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+
+    //unregister
+    public void doDelete(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+}
